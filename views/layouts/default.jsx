16547c1d45990f87886cf39fbed3d0516039dc43
Katie Eells <katieeells@Katies-MBP.lan>
2022-11-22 17:26:52 -0500

    started default.jsx code

---
+++ views/layouts/default.jsx
@@ -0,0 +1,25 @@
+// require react to utilize JSX
+const React = require('react')
+
+// Write a function called Default to hold an HTML boilerplate w/
+// one argument (html)
+
+function Default (html) {
+    return (
+        <html lang="en">
+            <head>
+                <title>Default</title>
+            </head>
+            <body>
+                <h1>HTML Rendered!</h1>
+                <div className='container'></div>
+                {/* the the code where to load the HTML children for the different views
+                the {} will tell React/JSX to stop, evaluate and render contents */}
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+// export the code to other files
+module.exports = Default
