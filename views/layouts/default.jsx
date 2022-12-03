0b279b18a6d77bb2676efc1496fa2576d02380f7
Katie Eells <katieeells@Katies-MBP.lan>
2022-12-03 16:37:51 -0500

    added gluten info to show and some navigation to home page

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -16,10 +16,18 @@ function Default(html) {
                 <title>{html.title || 'Default'}</title>
             </head>
             <body>
-                <h1>HTML Rendered!</h1>
+                <div className='wrapper'>
+                    <header>
+                        <h1>
+                            <a href="/breads">BreadCRUD</a>
+                        </h1>
+                    </header>
+                    <div className='container'>
                     {/* the code to tell where to load the HTML children for the different views
                     the {} will tell React/JSX to stop, evaluate and render contents */}    
                     {html.children}
+                    </div>
+                </div>
             </body>
         </html>
     )
--- views/show.jsx
+++ views/show.jsx
@@ -5,9 +5,20 @@ function Show ({bread}) {
     // console.log(bread.name)
         return (
             <Default>
-                <h2>Show Page</h2>
                 <h3>{bread.name}</h3>
+                {/* below is a ternary operator (if/else are not available in JSX) about gluten info */}
+                <p>
+                    and it 
+                    {
+                        bread.hasGluten 
+                        ? <span> does </span>
+                        : <span> does NOT </span>
+                    }
+                    have gluten
+                </p>
                 <img src={bread.image} alt={bread.name} />
+                {/* link to home */}
+                <li><a href="/breads">Go Home</a></li>
             </Default>
         )
 }
