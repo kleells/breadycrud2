4c2b508c8a1cfa461ab8e1ad611dc225a8cde316
Katie Eells <katieeells@Katies-MBP.lan>
2022-12-11 14:09:31 -0500

    added favicon

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -37,6 +37,7 @@ const React = require('react')
         return (
             <html>
                 <head>
+                    <link rel="shortcut icon" href="/images/favicon.ico" />
                     <title>{html.title || 'Default'}</title>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" />
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" integrity="sha512-EZLkOqwILORob+p0BXZc+Vm3RgJBOe1Iq/0fiI7r/wJgzOFZMlsqTa29UEl6v6U6gsV4uIpsNZoV32YZqrCRCQ==" crossOrigin="anonymous" />
